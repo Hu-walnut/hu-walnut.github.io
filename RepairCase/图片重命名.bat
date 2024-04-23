df138bf11acf7a494493755306efb22f51fb2375
Hu-walnut <2452196602@qq.com>
2024-04-23 20:15:02 +0800

    Site updated: 2024-04-23 20:15:01

--- RepairCase/图片重命名.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /r %%a in (*.txt) do (
-     for /f "tokens=2 delims=[]" %%b in ("%%~na") do ren "%%a" [%%b].txt
-     )
-pause
--- RepairCase/文件夹创建.bat
+++
@@ -1,8 +0,0 @@
-md kitchen
-md masterBedroom
-md other
-md parlor
-md restaurant
-md secondaryRecumbent
-md toilet
-md floorPlan
